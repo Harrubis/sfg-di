811ed572dc66283198ebe1e919872e2180721482
Nikita Mamets <namamets@rgs.ru>
2022-08-16 09:07:39 +0300

    Added 2 services for different profiles, added spring.active.profile to application.properties for choosing one of them

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,9 +14,14 @@ public class SfgDiApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+        String helloString = "";
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        helloString = i18nController.sayHello();
+        log.info(helloString);
+
 
         MyController myController = (MyController) ctx.getBean("myController");
-        String helloString = myController.sayHello();
+        helloString = myController.sayHello();
         log.info("--- Primary bean");
         log.info(helloString);
 
